4018a48caeaa47d0727a1fab2565e11325b677db
Zasderti <90847997+PorrettiAaron@users.noreply.github.com>
2025-05-21 19:49:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ function login(event) {
   usuarioRegistrado = true;
   document.getElementById('navbar').classList.remove('oculto');
   mostrarSeccion('noticias');
-  mostrarFormularioPreguntas();
+  //mostrarFormularioPreguntas();
 
   const btnCrear = document.getElementById('btn-crear-noticia');
+  console.log(tipoUsuario)
   if (tipoUsuario === 'admin') {
     btnCrear.style.display = 'inline-block';
   } else {
@@ -41,7 +42,9 @@ function mostrarSeccion(id) {
 }
 
 function mostrarFormularioPreguntas() {
+  console.log("Entre a mostrar formulario")
   if (usuarioRegistrado) {
+    console.log("Entre al if de usuario registrado")
     document.getElementById('form-pregunta').classList.remove('oculto');
   }
 }
